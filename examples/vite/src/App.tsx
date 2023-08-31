09dfd72f46edd5ce70bda7a079d19e9510416c2b
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-09-01 01:19:37 +0530

    vite example

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { ForkMe } from "@mayank1513/fork-me/server";
 import { ThemeSwitcher } from "react18-themes";
+import { ForkMe } from "@mayank1513/fork-me/server";
 import "./App.css";
 
 function App() {

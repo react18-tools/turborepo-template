e98ad054e67b7657a206a478e2043bf23d30c4cd
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-08-31 23:21:13 +0530

    update vite example and editor config

--- examples/vite/src/App.tsx
+++ examples/vite/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { ForkMe } from "@mayank1513/fork-me/server";
+import { ThemeSwitcher } from "react18-themes";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 
 	return (
 		<>
+			<ThemeSwitcher />
 			<div>
 				<a href="https://vitejs.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />

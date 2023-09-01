a461284e5892a35deeec9b98ff4502e6d5aaa911
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-09-01 21:12:44 +0530

    update generators and css

--- packages/fork-me/turbo/generators/config.ts
+++ packages/fork-me/turbo/generators/config.ts
@@ -1,4 +1,6 @@
 import type { PlopTypes } from "@turbo/gen";
+import fs from "fs";
+import path from "path";
 
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
 	// A simple generator to add a new React component to the internal UI library
@@ -22,8 +24,44 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 			},
 		],
 		actions: data => {
-			const root = data?.isClient ? "src/client/" : "src/server/";
-			return [
+			let root = data?.isClient ? "src/client/" : "src/server/";
+			const _actions: PlopTypes.ActionType[] = [];
+			if (data?.name.includes("/")) {
+				const name = data.name as string;
+				const lastSlashInd = name.lastIndexOf("/") || name.lastIndexOf("\\");
+				data.name = name.slice(lastSlashInd + 1);
+				const dir = name.slice(0, lastSlashInd).split(/\/|\\/);
+				const r1 = root.split(/\/|\\/);
+				for (let i = 1; i <= dir.length; i++) {
+					const p = path.resolve(__dirname, "..", "..", ...r1, ...dir.slice(0, i), "index.ts");
+					if (!fs.existsSync(p)) {
+						console.log(p);
+						const content = `${data?.isClient ? '"use client";\n' : ""}// ${dir
+							.slice(0, i)
+							.join("/")} component exports\n`;
+						_actions.push({
+							type: "add",
+							path: root + dir.slice(0, i).join("/") + "/index.ts",
+							template: content,
+						});
+						_actions.push({
+							type: "append",
+							pattern: /(?<insertion> component exports)/g,
+							path: root + (i === 1 ? "" : dir.slice(0, i - 1).join("/") + "/") + "index.ts",
+							template: `export * from "./${dir[i - 1]}"`,
+						});
+					}
+				}
+				root = root + dir.join("/") + "/";
+			}
+			return _actions.concat([
+				{
+					type: "add",
+					path: root + "{{kebabCase name}}/index.ts",
+					template: `${
+						data?.isClient ? '"use client";\n\n' : ""
+					}export * from "./{{kebabCase name}}";\n`,
+				},
 				{
 					type: "add",
 					path: root + "{{kebabCase name}}/{{kebabCase name}}.tsx",
@@ -35,19 +73,12 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
 					templateFile: "templates/component.test.hbs",
 				},
 				{
-					type: "add",
-					path: root + "{{kebabCase name}}/index.tsx",
-					template: `${
-						data?.isClient ? '"use client";\n\n' : ""
-					}export * from "./{{kebabCase name}}";\n`,
-				},
-				{
 					type: "append",
-					path: root + "index.tsx",
+					path: root + "index.ts",
 					pattern: /(?<insertion> component exports)/g,
 					template: 'export * from "./{{kebabCase name}}";',
 				},
-			];
+			]);
 		},
 	});
 }

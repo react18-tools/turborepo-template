c11801f682cdfa1387f5bb5b8f4ade90bc63bd9a
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2023-12-03 12:24:12 +0530

    vite [skip ci]

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import { SharedRootLayout } from "shared-ui";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<SharedRootLayout>
+			<App />
+		</SharedRootLayout>
 	</React.StrictMode>,
 );

6515880cb084c9ea480011289f61397623a69575
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-03-04 11:43:07 +0530

    delete preinstall file

--- preinstall.js
+++ preinstall.js
@@ -1,7 +1,6 @@
 // Run bash script
 const fs = require("fs");
 const path = require("path");
-require("child_process").spawn("bash", ["./preinstall.sh"], { stdio: "inherit" });
 
 const readMePath = path.resolve(process.cwd(), "README.md");
 let readMe = fs.readFileSync(readMePath, "utf8");
@@ -9,3 +8,9 @@ readMe = readMe.replace(/## Features(?:.|\n|\r)*#### Create a library that is/m,
 readMe = readMe.replace(/## Introduction(?:.|\n|\r)*## Step/m, "## Step");
 
 fs.writeFileSync(readMePath, readMe);
+
+/** run the old bash script */
+require("child_process").spawn("bash", ["./preinstall.sh"], { stdio: "inherit" });
+
+/** delete this file */
+fs.unlinkSync(path.resolve(process.cwd(), "preinstall.js"));

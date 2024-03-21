2caf18d7abe1c1bd578fbe9d697d21d52e4aaf1e
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-03-21 15:03:12 +0530

    Fix deepsource antipattern

--- examples/vite/src/main.tsx
+++ examples/vite/src/main.tsx
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { SharedRootLayout } from "shared-ui";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) throw new Error('Element with id="root" not found.');
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <SharedRootLayout>
       <App />

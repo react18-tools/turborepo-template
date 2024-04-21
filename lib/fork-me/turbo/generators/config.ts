03a582b707c6f7b0ec2df813b1d15361ea94706a
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-04-21 12:11:13 +0530

    Fix anti-patterns

--- lib/fork-me/turbo/generators/config.ts
+++ lib/fork-me/turbo/generators/config.ts
@@ -2,89 +2,9 @@ import fs from "node:fs";
 import path from "node:path";
 import type { PlopTypes } from "@turbo/gen";
 
-export default function generator(plop: PlopTypes.NodePlopAPI): void {
-  // A simple generator to add a new React component to the internal UI library
-  plop.setGenerator("react-component", {
-    description: "Adds a new react component",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the component?",
-      },
-      {
-        type: "confirm",
-        name: "isClient",
-        message: 'Is this a client component? (Should we add "use client" directive?)',
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Describe your component. (This will be added as js-doc comment.)",
-      },
-    ],
-    actions: data => (data ? getActions(data as InquirerDataType) : []),
-  });
-}
-
 interface InquirerDataType {
   isClient: boolean;
   name: string;
-}
-
-function getActions(data: InquirerDataType) {
-  const { nestedRouteActions, root } = getNestedRouteActions(data);
-  return nestedRouteActions.concat([
-    {
-      type: "add",
-      path: `${root}{{kebabCase name}}/index.ts`,
-      template: `${
-        data.isClient ? '"use client";\n\n' : ""
-      }export * from "./{{kebabCase name}}";\n`,
-    },
-    {
-      type: "add",
-      path: `${root}{{kebabCase name}}/{{kebabCase name}}.tsx`,
-      templateFile: "templates/component.hbs",
-    },
-    {
-      type: "add",
-      path: `${root}{{kebabCase name}}/{{kebabCase name}}.test.tsx`,
-      templateFile: "templates/component.test.hbs",
-    },
-    {
-      type: "add",
-      path: `${root}{{kebabCase name}}/{{kebabCase name}}.module.scss`,
-      templateFile: "templates/component.module.hbs",
-    },
-    {
-      type: "append",
-      path: `${root}index.ts`,
-      pattern: /(?<insertion> component exports)/g,
-      template: 'export * from "./{{kebabCase name}}";',
-    },
-  ]);
-}
-
-function getNestedRouteActions(data: InquirerDataType) {
-  const { isClient, name } = data;
-  const root = isClient ? "src/client/" : "src/server/";
-  const nestedRouteActions: PlopTypes.ActionType[] = [];
-
-  /** Return early if no nested routes */
-  if (!name.includes("/")) return { nestedRouteActions, root };
-
-  const lastSlashInd = name.lastIndexOf("/") || name.lastIndexOf("\\");
-  /** following is required to make sure appropreate name is used while creating components */
-  data.name = name.slice(lastSlashInd + 1);
-
-  const directories = name.slice(0, lastSlashInd).split(/\/|\\/);
-  const rootSegments = [...root.split(/\/|\\/)];
-
-  for (let i = 1; i <= directories.length; i++)
-    updateIndexFilesIfNeeded(nestedRouteActions, rootSegments, directories.slice(0, i), isClient);
-
-  return { nestedRouteActions, root: `${root + directories.join("/")}/` };
 }
 
 function updateIndexFilesIfNeeded(
@@ -121,4 +41,84 @@ function updateIndexFilesIfNeeded(
       template: `export * from "./${currentDirSegments[length - 1]}"`,
     });
   }
+}
+
+function getNestedRouteActions(data: InquirerDataType) {
+  const { isClient, name } = data;
+  const root = isClient ? "src/client/" : "src/server/";
+  const nestedRouteActions: PlopTypes.ActionType[] = [];
+
+  /** Return early if no nested routes */
+  if (!name.includes("/")) return { nestedRouteActions, root };
+
+  const lastSlashInd = name.lastIndexOf("/") || name.lastIndexOf("\\");
+  /** following is required to make sure appropreate name is used while creating components */
+  data.name = name.slice(lastSlashInd + 1);
+
+  const directories = name.slice(0, lastSlashInd).split(/\/|\\/);
+  const rootSegments = [...root.split(/\/|\\/)];
+
+  for (let i = 1; i <= directories.length; i++)
+    updateIndexFilesIfNeeded(nestedRouteActions, rootSegments, directories.slice(0, i), isClient);
+
+  return { nestedRouteActions, root: `${root + directories.join("/")}/` };
+}
+
+function getActions(data: InquirerDataType) {
+  const { nestedRouteActions, root } = getNestedRouteActions(data);
+  return nestedRouteActions.concat([
+    {
+      type: "add",
+      path: `${root}{{kebabCase name}}/index.ts`,
+      template: `${
+        data.isClient ? '"use client";\n\n' : ""
+      }export * from "./{{kebabCase name}}";\n`,
+    },
+    {
+      type: "add",
+      path: `${root}{{kebabCase name}}/{{kebabCase name}}.tsx`,
+      templateFile: "templates/component.hbs",
+    },
+    {
+      type: "add",
+      path: `${root}{{kebabCase name}}/{{kebabCase name}}.test.tsx`,
+      templateFile: "templates/component.test.hbs",
+    },
+    {
+      type: "add",
+      path: `${root}{{kebabCase name}}/{{kebabCase name}}.module.scss`,
+      templateFile: "templates/component.module.hbs",
+    },
+    {
+      type: "append",
+      path: `${root}index.ts`,
+      pattern: /(?<insertion> component exports)/g,
+      template: 'export * from "./{{kebabCase name}}";',
+    },
+  ]);
+}
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  // A simple generator to add a new React component to the internal UI library
+  plop.setGenerator("react-component", {
+    description: "Adds a new react component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "What is the name of the component?",
+      },
+      {
+        type: "confirm",
+        name: "isClient",
+        message: 'Is this a client component? (Should we add "use client" directive?)',
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Describe your component. (This will be added as js-doc comment.)",
+      },
+    ],
+    actions: data => (data ? getActions(data as InquirerDataType) : []),
+  });
 }

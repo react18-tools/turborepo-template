dc701bfc4ee0e098096b4faa31db3c2b9e40dc32
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-04-24 13:37:45 +0530

    Setup tsup config

--- lib/tsup.config.ts
+++ lib/tsup.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig, type Options } from "tsup";
+import react18Plugin from "esbuild-plugin-react18";
+import cssPlugin from "esbuild-plugin-react18-css";
 
-export default defineConfig((options: Options) => ({
-  entry: ["./src/index.tsx"],
-  format: ["cjs", "esm"],
-  dts: true,
-  external: ["react"],
-  banner: {
-    js: "'use client'",
-  },
-  ...options,
-}));
+export default defineConfig(
+  (options: Options) =>
+    ({
+      format: ["cjs", "esm"],
+      target: "es2019",
+      sourcemap: false,
+      clean: true,
+      bundle: true,
+      minify: !options.watch,
+      esbuildPlugins: [react18Plugin(), cssPlugin()],
+      dts: true,
+      external: ["react"],
+      ...options,
+    }) as Options,
+);

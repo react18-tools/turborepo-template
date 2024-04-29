0a25f8bbe731aeecb22af556835d4b7b4a49b888
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-04-29 10:00:10 +0530

    Export hook from root index.ts

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,1 +1,8 @@
+/**
+ * Don't export client side components from here.
+ *
+ * Hooks are okay to export from here as they do not require "use client" directive.
+ */
+
 export * from "./server";
+export * from "./hooks";

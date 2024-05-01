e428a216d0476d6e92fd229811012f43bd663fd6
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-01 18:24:47 +0530

    Add theme libs

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -2,11 +2,15 @@ import "./styles.css";
 import { Bars2 } from "react18-loaders";
 import { LoaderContainer } from "react18-loaders/dist/client";
 import "react18-loaders/dist/index.css";
+import { NextJsServerTarget } from "nextjs-themes/server";
+import { ThemeSwitcher } from "nextjs-themes";
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
       <body>
+        <NextJsServerTarget />
+        <ThemeSwitcher />
         {children}
         <LoaderContainer>
           <Bars2 color="red" />

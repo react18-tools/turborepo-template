6e87eaa97cf6824ce8177c4987277b7a18858720
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-02 17:47:28 +0530

    Touch up

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "react18-loaders/dist/index.css";
 import { NextJsServerTarget } from "nextjs-themes/server";
 import { ThemeSwitcher } from "nextjs-themes";
 import { Layout } from "@repo/shared/dist/server";
-import { GlobalLoader } from "@repo/shared/dist/client";
+import { GlobalLoader } from "@repo/shared";
 import Header from "../components/header";
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
--- packages/shared/src/server/layout/layout.tsx
+++ packages/shared/src/server/layout/layout.tsx
@@ -14,7 +14,11 @@ export function Layout({ children }: LayoutProps) {
   return (
     <div className={styles.container}>
       {children}
-      <ForkMe gitHubUrl="https://github.com/react18-tools/turborepo-template" />
+      <ForkMe
+        gitHubUrl="https://github.com/react18-tools/turborepo-template"
+        bgColor="var(--text-color)"
+        textColor="var(--bg-color)"
+      />
       <footer>
         With 💖 by{" "}
         <a href="https://mayank-chaudhari.vercel.app/" target="_blank" rel="noopener noreferrer">

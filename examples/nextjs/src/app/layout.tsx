33a446dc5c11811a6cd33448b26229d37db4b03c
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-03 21:08:22 +0530

    Add inter fonts

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -4,11 +4,14 @@ import { NextJsServerTarget } from "nextjs-themes/server";
 import { ThemeSwitcher } from "nextjs-themes";
 import { Layout } from "@repo/shared/dist/server";
 import { GlobalLoader, Header } from "@repo/shared";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <NextJsServerTarget />
         <ThemeSwitcher />
         <Layout>

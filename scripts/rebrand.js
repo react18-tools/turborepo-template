73f619f3d9ee3f5ee8fb70b1b0ff2c63c3d7735f
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-04 18:56:26 +0530

    rebrand packages

--- scripts/rebrand.js
+++ scripts/rebrand.js
@@ -4,14 +4,53 @@ const rebrandConfig = require("./rebrand.config.json");
 const packageJSON = require("../lib/package.json");
 
 const rootDir = process.cwd();
+const oldPkgName = packageJSON.name;
 
-// Delete old changelogs
+const { packageName, owner, repo } = rebrandConfig;
+
+// Rebrand lib packageJSON
+packageJSON.name = packageName;
+packageJSON.description = "";
+packageJSON.version = "0.0.0";
+packageJSON.repository = `github:${owner}/${repo}`;
+packageJSON.funding.pop();
+packageJSON.bugs = `https://github.com/${owner}/${repo}/issues`;
+packageJSON.homepage = `https://github.com/${owner}/${repo}/#readme`;
+packageJSON.funding.unshift({
+  type: "github",
+  url: `https://github.com/sponsors/${owner}`,
+});
+packageJSON.keywords = packageJSON.keywords.slice(2);
+
+fs.writeFileSync(
+  path.resolve(rootDir, "lib", "package.json"),
+  JSON.stringify(packageJSON, null, 2),
+);
+
+const updatePkgAndRemoveChangelogs = dir => {
+  // update package.json for packages and examples
+  const pkgPath = path.resolve(dir, "package.json");
+  const pkg = JSON.parse(fs.readFileSync(pkgPath));
+  if (pkg.dependencies[oldPkgName]) {
+    pkg.dependencies[oldPkgName] = "latest";
+    pkg.dependencies[packageJSON.name] = "workspace:*";
+  } else if (pkg.devDependencies[oldPkgName]) {
+    pkg.devDependencies[oldPkgName] = "latest";
+    pkg.dependencies[packageJSON.name] = "workspace:*";
+  }
+
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
+
+  // Delete old changelogs
+  try {
+    fs.unlinkSync(path.resolve(dir, "CHANGELOG.md"));
+  } catch {}
+};
+
 ["examples", "packages"].forEach(dir => {
-  fs.readdirSync(path.resolve(rootDir, dir)).forEach(f => {
-    try {
-      fs.unlinkSync(path.resolve(rootDir, dir, f, "CHANGELOG.md"));
-    } catch {}
-  });
+  fs.readdirSync(path.resolve(rootDir, dir)).forEach(f =>
+    updatePkgAndRemoveChangelogs(path.resolve(rootDir, dir, f)),
+  );
 });
 
 try {
@@ -20,7 +59,13 @@ try {
 
 if (!rebrandConfig.keepRebrandingScripts) {
   const rootPackageJSON = require("../package.json");
+  delete rootPackageJSON.scripts.rebrand;
+  delete rootPackageJSON.scripts.postinstall;
   try {
+    fs.writeFileSync(
+      path.resolve(rootDir, "package.json"),
+      JSON.stringify(rootPackageJSON, null, 2),
+    );
     fs.unlinkSync(path.resolve(rootDir, "rebrand.config.json"));
     fs.unlinkSync(path.resolve(rootDir, "rebrand.js"));
   } catch (e) {

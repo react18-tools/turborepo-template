0222cc2c9577b97f98b17694cb81afa36c495682
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-04 10:27:03 +0530

    add react-live

--- packages/shared/src/server/landing-page/landing-page.test.tsx
+++ packages/shared/src/server/landing-page/landing-page.test.tsx
@@ -5,7 +5,7 @@ import { LandingPage } from "./landing-page";
 describe.concurrent("landing-page", () => {
   afterEach(cleanup);
 
-  test("check if renders without errors", ({ expect }) => {
+  test("check if renders without errors", ({}) => {
     render(<LandingPage title="MyTitle" />);
   });
 });

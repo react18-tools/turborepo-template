cf2c17084e76be23a08defed4620398b4325141f
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-04 15:31:19 +0530

    Updae docs config and docs

--- typedoc.config.js
+++ typedoc.config.js
@@ -3,11 +3,11 @@ module.exports = {
   tsconfig: "tsconfig.docs.json",
   name: "React18 Loaders",
   entryPoints: ["./lib/src"],
+  exclude: ["**/*.test.tsx", "**/index.ts", "**/declaration.d.ts"],
   entryPointStrategy: "Expand",
   out: "./docs",
   commentStyle: "all",
   searchInComments: true,
-  includeVersion: true,
   excludeExternals: true,
   plugin: [
     "typedoc-plugin-mdn-links",

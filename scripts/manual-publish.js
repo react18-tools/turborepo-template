b39361eadc2798837f219c26e48f40d6f9f18fab
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-07 11:31:09 +0530

    Update publish

---
+++ scripts/manual-publish.js
@@ -0,0 +1,66 @@
+/**
+ * allow only patch changes from release branches.
+ * Major and minor changes allowed only from main branch.
+ * pre-release only from branch containing dev or alpha in the branchname
+ */
+
+/** Let the following error be thrown by npm. There are situations where publish could have failed for different reasons. */
+// throws an exception if process.env.oldv === process.env.v The library version is not up to date, error(" Not able to release to the same version.
+
+const { execSync } = require("child_process");
+
+const BRANCH = process.env.BRANCH;
+const DEFAULT_BRANCH = process.env.DEFAULT_BRANCH;
+
+const isLatestRelease = BRANCH === DEFAULT_BRANCH || BRANCH.includes("release-");
+let tag = "latest";
+
+const OLD_VERSION = require("../lib/package.json").version;
+if (!isLatestRelease) {
+  /** pre-release branch name should be the tag name (e.g., beta, canery, etc.) or tag name followed by a '-' and version or other specifiers. e.g. beta-2.0 */
+  tag = BRANCH.split("-")[0];
+  try {
+    execSync(`pnpm changeset pre enter ${tag}`);
+  } catch (e) {
+    console.log({ e });
+  }
+}
+/** Apply changeset */
+execSync("pnpm changeset version");
+const NEW_VERSION = require("../lib/package.json").version;
+
+// exit pre mode -- to avoid collision with full releases
+execSync("pnpm changeset pre exit");
+
+const [newMajor, newMinor] = NEW_VERSION.split(".");
+const [oldMajor, oldMinor] = OLD_VERSION.split(".");
+
+const isNotPatch = newMajor !== oldMajor && newMinor !== oldMinor;
+
+const pushCmd = `git add . && git commit -m "Apply changesets and update CHANGELOG" && git push origin ${BRANCH}`;
+
+if (isNotPatch && BRANCH === DEFAULT_BRANCH) {
+  execSync(pushCmd);
+  /** Create new release branch for every Major or Minor release */
+  const releaseBranch = `release-${newMajor}.${newMinor}`;
+  execSync(`git checkout -b ${releaseBranch} && git push origin ${releaseBranch}`);
+} else if (isLatestRelease) {
+  /** New version must be valid SEMVER version. No pre-release (beta/alpha etc.) */
+  if (!/^\d+\.\d+.\d+$/.test(NEW_VERSION)) throw new Error("Invalid version");
+
+  if (isNotPatch)
+    throw new Error("Major or Minor changes can be published only from the default branch.");
+
+  // Push changes back to the repo
+  execSync(pushCmd);
+} else {
+  execSync(pushCmd);
+}
+
+/** Create release */
+execSync(`cd lib && pnpm build && npm publish --provenance --access public --tag ${tag}`);
+
+/** Create GitHub release */
+execSync(
+  `gh release create ${NEW_VERSION} --generate-notes${isLatestRelease ? " --latest" : ""} -n "$(sed '1,/^## /d;/^## /,$d' CHANGELOG.md)" --title "Release v${NEW_VERSION}"`,
+);
--- scripts/publish.js
+++ scripts/publish.js
@@ -1,66 +1,32 @@
-/**
- * allow only patch changes from release branches.
- * Major and minor changes allowed only from main branch.
- * pre-release only from branch containing dev or alpha in the branchname
- */
-
-/** Let the following error be thrown by npm. There are situations where publish could have failed for different reasons. */
-// throws an exception if process.env.oldv === process.env.v The library version is not up to date, error(" Not able to release to the same version.
-
+/** It is assumed that this is called only from the default branch. */
 const { execSync } = require("child_process");
 
-const BRANCH = process.env.BRANCH;
-const DEFAULT_BRANCH = process.env.DEFAULT_BRANCH;
+// Apply changesets if any -- e.g., coming from pre-release branches
+execSync("pnpm changeset pre exit && pnpm changeset version");
+execSync(
+  `git add . && git commit -m "Apply changesets and update CHANGELOG" && git push origin ${process.env.BRANCH}`,
+);
 
-const isLatestRelease = BRANCH === DEFAULT_BRANCH || BRANCH.includes("release-");
-let tag = "latest";
+const LATEST_VERSION = process.env.LATEST_VERSION;
+const VERSION = require("../lib/package.json").version;
 
-const OLD_VERSION = require("../lib/package.json").version;
-if (!isLatestRelease) {
-  /** pre-release branch name should be the tag name (e.g., beta, canery, etc.) or tag name followed by a '-' and version or other specifiers. e.g. beta-2.0 */
-  tag = BRANCH.split("-")[0];
-  try {
-    execSync(`pnpm changeset pre enter ${tag}`);
-  } catch (e) {
-    console.log({ e });
-  }
-}
-/** Apply changeset */
-execSync("pnpm changeset version");
-const NEW_VERSION = require("../lib/package.json").version;
+console.log({ VERSION, LATEST_VERSION });
 
-// exit pre mode -- to avoid collision with full releases
-execSync("pnpm changeset pre exit");
-
-const [newMajor, newMinor] = NEW_VERSION.split(".");
-const [oldMajor, oldMinor] = OLD_VERSION.split(".");
+const [newMajor, newMinor] = VERSION.split(".");
+const [oldMajor, oldMinor] = LATEST_VERSION.split(".");
 
 const isNotPatch = newMajor !== oldMajor && newMinor !== oldMinor;
 
-const pushCmd = `git add . && git commit -m "Apply changesets and update CHANGELOG" && git push origin ${BRANCH}`;
-
-if (isNotPatch && BRANCH === DEFAULT_BRANCH) {
-  execSync(pushCmd);
+if (isNotPatch) {
   /** Create new release branch for every Major or Minor release */
   const releaseBranch = `release-${newMajor}.${newMinor}`;
   execSync(`git checkout -b ${releaseBranch} && git push origin ${releaseBranch}`);
-} else if (isLatestRelease) {
-  /** New version must be valid SEMVER version. No pre-release (beta/alpha etc.) */
-  if (!/^\d+\.\d+.\d+$/.test(NEW_VERSION)) throw new Error("Invalid version");
-
-  if (isNotPatch)
-    throw new Error("Major or Minor changes can be published only from the default branch.");
-
-  // Push changes back to the repo
-  execSync(pushCmd);
-} else {
-  execSync(pushCmd);
 }
 
 /** Create release */
-execSync(`cd lib && pnpm build && npm publish --provenance --access public --tag ${tag}`);
+execSync("cd lib && pnpm build && npm publish --provenance --access public");
 
 /** Create GitHub release */
 execSync(
-  `gh release create ${NEW_VERSION} --generate-notes${isLatestRelease ? " --latest" : ""} -n "$(sed '1,/^## /d;/^## /,$d' CHANGELOG.md)" --title "Release v${NEW_VERSION}"`,
+  `gh release create ${VERSION} --generate-notes --latest -n "$(sed '1,/^## /d;/^## /,$d' CHANGELOG.md)" --title "Release v${VERSION}"`,
 );

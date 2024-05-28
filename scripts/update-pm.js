fae9f7ffb649471aa93b66a26afc1d915649ea39
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-28 06:37:02 +0530

    Create a commit after updating package manager

--- scripts/update-pm.js
+++ scripts/update-pm.js
@@ -7,3 +7,10 @@ const PNPM_VERSION = execSync("pnpm -v").toString().trim();
 packageJSON.packageManager = `pnpm@${PNPM_VERSION}`;
 
 fs.writeFileSync(path.resolve(__dirname, "../package.json"), JSON.stringify(packageJSON, null, 2));
+
+// commit to repo
+try {
+  execSync("git add . && git commit -m 'Update package.json with pnpm version'");
+} catch {
+  // no changesets to be applied
+}

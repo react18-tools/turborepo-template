a4b653bc1af6e25697bb015270e1d3533a14dc28
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-30 16:45:19 +0530

    Remove uuid as it will result in multiple rgs stores for treeshakable imports

--- lib/src/constants.ts
+++ lib/src/constants.ts
@@ -1,3 +1,3 @@
 /** generate uuid to avoid conflicting keys */
-const uuid = () => (Date.now() * Math.random()).toString(16).slice(2, 8);
-export const LOADER_RGS_KEY = `loader-${uuid()}`;
+// const uuid = () => (Date.now() * Math.random()).toString(16).slice(2, 8);
+export const LOADER_RGS_KEY = "r18-loaders";

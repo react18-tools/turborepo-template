d4147bebd38ca3d7d047b99ea4f40ee455aed392
Mayank <mayank.srmu@gmail.com>
2024-06-07 22:30:48 +0530

    feat: Add new prop to Loader container - Add prop loading - Improve styles and provide fade effect

--- lib/src/client/loader-container/loader-container.tsx
+++ lib/src/client/loader-container/loader-container.tsx
@@ -4,13 +4,17 @@ import { LOADER_RGS_KEY } from "../../constants";
 
 interface LoaderContainerProps extends React.HTMLProps<HTMLDivElement> {
   children?: React.ReactNode;
+  loading?: boolean;
 }
 
 /**
  * # LoaderContainer
  * A full screen container for the loading animation.
  */
-export function LoaderContainer({ children }: LoaderContainerProps) {
-  const [loading] = useRGS(LOADER_RGS_KEY, false);
-  return loading ? <div className={styles.container}>{children}</div> : null;
+export function LoaderContainer({ children, loading }: LoaderContainerProps) {
+  const [_loading] = useRGS(LOADER_RGS_KEY, false);
+  const loading_ = loading ?? _loading;
+  return (
+    <div className={[styles.container, loading_ ? styles.loading : ""].join(" ")}>{children}</div>
+  );
 }

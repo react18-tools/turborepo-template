31e7f9977b0bfe6c6510f2320b1ec2d28d526278
Mayank <mayank.srmu@gmail.com>
2024-06-09 22:28:55 +0530

    Update vite example

--- examples/vite/src/app/index.tsx
+++ examples/vite/src/app/index.tsx
@@ -2,26 +2,26 @@ import React, { useCallback } from "react";
 import "./styles.css";
 import { Bars1, Bars2, Dots1, Dots2 } from "react18-loaders/dist/server";
 import { LoaderContainer, useLoader } from "react18-loaders";
+import { LandingPage, Layout } from "@repo/shared/dist/server";
+import { Core } from "nextjs-darkmode";
+import { Demo, Header } from "@repo/shared";
 
 /** Vite App */
 function App(): JSX.Element {
   const { setLoading } = useLoader();
   const handleClick = useCallback(() => setLoading(true), []);
   return (
-    <div className="container">
-      <h1 className="title">
-        Admin <br />
-        <span>Turborepo Template</span>
-      </h1>
-      <Dots1 color="#00f" width={60} />
-      <Dots2 color="#00f" width={50} dotRadius={8} />
-      <Bars2 color="red" width={50} />
-      <Bars1 color="red" width={50} />
+    <Layout>
+      <Core t="background .5s" />
+      <Header />
+      <LandingPage title="Vite Example">
+        <Demo />
+      </LandingPage>
       <button onClick={handleClick}>Show loader</button>
       <LoaderContainer>
         <Bars1 color="red" width={50} />
       </LoaderContainer>
-    </div>
+    </Layout>
   );
 }
 

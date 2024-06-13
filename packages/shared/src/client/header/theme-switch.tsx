08fa77191ecba12cef484e6b172e4aebd1ecc40a
Mayank <mayank.srmu@gmail.com>
2024-06-13 13:55:21 +0530

    Update theme-switch

--- packages/shared/src/client/header/theme-switch.tsx
+++ packages/shared/src/client/header/theme-switch.tsx
@@ -1,25 +1,15 @@
 "use client";
 
 import { useMode } from "nextjs-darkmode/hooks";
-import styles from "./header.module.scss";
-import { useCallback } from "react";
-import type { ColorSchemePreference } from "nextjs-darkmode";
 import { Switch } from "nextjs-darkmode/switch";
-
-const modes: ColorSchemePreference[] = ["dark", "light", "system"];
+import styles from "./header.module.scss";
 
 /** This is a wrapper around `nextjs-darkmode's ColorSwitch component to improve mobile view. */
-export default function ThemeSwitch() {
-  const { mode, setMode } = useMode();
-  const toggle = useCallback(() => {
-    const index = modes.indexOf(mode);
-    setMode(modes[(index + 1) % modes.length]);
-  }, [mode]);
-
+export default function ThemeSwitch(): JSX.Element {
+  const { mode } = useMode();
   return (
-    <button className={styles.themeswitch} onClick={toggle}>
-      <Switch tag="div" />
+    <Switch className={styles.themeswitch}>
       <span className="mb">{mode}</span>
-    </button>
+    </Switch>
   );
 }

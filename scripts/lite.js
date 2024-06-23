cb8b5551933056450bce0fa621f33c39ea65beee
Mayank <mayank.srmu@gmail.com>
2024-06-23 18:29:20 +0530

    fix: fix lite script

--- scripts/lite.js
+++ scripts/lite.js
@@ -9,7 +9,7 @@ const packageJson = require("../lib/package.json");
 const ref = packageJson.name;
 packageJson.peerDependencies.r18gs = `${packageJson.dependencies.r18gs.split(".")[0]}`;
 delete packageJson.dependencies.r18gs;
-if (Object.keys(packageJson.devDependencies).length === 0) delete packageJson.devDependencies;
+if (Object.keys(packageJson.dependencies).length === 0) delete packageJson.dependencies;
 packageJson.name = `${ref}-lite`;
 
 fs.writeFileSync(

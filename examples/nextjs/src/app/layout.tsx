7978dd40e3399a78281fe3deb4c90b18a77fe2c5
Mayank <mayank.srmu@gmail.com>
2024-06-26 09:37:24 +0530

    Add mouse trails

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Layout } from "@repo/shared/dist/server";
 import { GlobalLoader, Header } from "@repo/shared";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { MouseTrail } from "react-mouse-trails";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }):
           {children}
         </Layout>
         <GlobalLoader />
+        <MouseTrail />
       </body>
     </html>
   );

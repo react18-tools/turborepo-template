4be4afcfb32ce3d4d831bfc758e1ed1e133c5ded
Mayank <mayank.srmu@gmail.com>
2024-07-20 07:52:59 +0530

    Add rdiPlugin to shared package

--- packages/shared/tsup.config.ts
+++ packages/shared/tsup.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 import react18Plugin from "esbuild-plugin-react18";
 import cssPlugin from "esbuild-plugin-react18-css";
+import { rdiPlugin } from "esbuild-plugin-rdi";
 
 export default defineConfig(
   (options: Options) =>
@@ -12,7 +13,11 @@ export default defineConfig(
       clean: !options.watch,
       bundle: true,
       minify: !options.watch,
-      esbuildPlugins: [react18Plugin(), cssPlugin({ generateScopedName: "[folder]__[local]" })],
+      esbuildPlugins: [
+        react18Plugin(),
+        cssPlugin({ generateScopedName: "[folder]__[local]" }),
+        rdiPlugin(),
+      ],
       external: ["react"],
       ...options,
     }) as Options,

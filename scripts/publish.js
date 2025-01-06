14d1d38cc8f3fded9a15f4f7ab0387fa77224dc3
Mayank <mayank.srmu@gmail.com>
2025-01-06 17:59:09 +0530

    Create release without Changelog if changelog does not exist

--- scripts/publish.js
+++ scripts/publish.js
@@ -53,9 +53,13 @@ const provenance = visibility.toLowerCase() === "public" ? "--provenance" : "";
 execSync(`cd lib && pnpm build && npm publish ${provenance} --access public`);
 
 /** Create GitHub release */
-execSync(
-  `gh release create ${VERSION} --generate-notes --latest -n "$(sed '1,/^## /d;/^## /,$d' CHANGELOG.md)" --title "Release v${VERSION}"`,
-);
+try {
+  execSync(
+    `gh release create ${VERSION} --generate-notes --latest -n "$(sed '1,/^## /d;/^## /,$d' CHANGELOG.md)" --title "Release v${VERSION}"`,
+  );
+} catch {
+  execSync(`gh release create ${VERSION} --generate-notes --latest --title "Release v${VERSION}"`);
+}
 
 execSync("node ./scripts/lite.js");
 execSync(`cd lib && pnpm build && npm publish ${provenance} --access public`);

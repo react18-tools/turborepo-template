a9b12f545f131710de374c497cad75ffc397f978
Mayank <mayank.srmu@gmail.com>
2025-05-02 22:32:48 +0530

    fix canonicals

--- scripts/publish-canonical.js
+++ scripts/publish-canonical.js
@@ -2,6 +2,6 @@ const { execSync } = require("child_process");
 
 // Publish canonical packages
 [].forEach(pkg => {
-  execSync(`sed -i -e "s/name.*/name\\": \\"${pkg}\\",/" lib/package.json`);
+  execSync(`sed -i -e "s/name.*/name\\": \\"${pkg.replace(/\//g, "\\/")}\\",/" lib/package.json`);
   execSync("cd lib && npm publish --provenance --access public");
 });

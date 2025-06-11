a0fcbda0d0484b102fbb8fba362e4aad40d7e084
Mayank <mayank.srmu@gmail.com>
2025-06-11 10:36:35 +0530

    Use try catch for updating security.md and release branch

--- scripts/publish.js
+++ scripts/publish.js
@@ -43,9 +43,13 @@ if (isPatch) {
     );
   } catch {}
 } else {
-  require("./update-security-md")(`${newMajor}.${newMinor}`, `${oldMajor}.${oldMinor}`);
-  /** Create new release branch for every Major or Minor release */
-  execSync(`git checkout -b ${releaseBranch} && git push origin ${releaseBranch}`);
+  try {
+    require("./update-security-md")(`${newMajor}.${newMinor}`, `${oldMajor}.${oldMinor}`);
+    /** Create new release branch for every Major or Minor release */
+    execSync(`git checkout -b ${releaseBranch} && git push origin ${releaseBranch}`);
+  } catch (err) {
+    console.error("Error pushing to release branch: ", err);
+  }
 }
 
 const { visibility } = JSON.parse(execSync("gh repo view --json visibility").toString());

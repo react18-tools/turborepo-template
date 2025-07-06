7dc24ed7c41cd655cdc57a1a434ad19c48e9ac08
Mayank <mayank.srmu@gmail.com>
2025-07-06 16:31:21 +0530

    feat: enhance package.json exports handling in rc.js

--- scripts/rc.js
+++ scripts/rc.js
@@ -12,22 +12,44 @@ const TEMPLATE_DIR = "scripts/templates/";
  */
 
 /**
+ * create exports entry
+ */
+function createExportsEntry(pkgJSON, target) {
+  const entry = {
+    types: `./dist/${target}/index.d.ts`,
+    import: `./dist/${target}/index.mjs`,
+    require: `./dist/${target}/index.js`,
+  };
+  pkgJSON.exports[`./${target}`] = entry;
+  pkgJSON.exports[`./dist/${target}`] = entry;
+  pkgJSON.exports[`./${target}/index.css`] = `./dist/${target}/index.css`;
+  pkgJSON.exports[`./dist/${target}/index.css`] = `./dist/${target}/index.css`;
+}
+
+/**
  * Updates index files if needed based on the provided parameters.
  * @param {import('plop').ActionType} nestedRouteActions - Nested route actions.
  * @param {string[]} rootSegments - Root segments.
  * @param {string[]} currentDirSegments - Current directory segments.
- * @param {boolean} isClient - Indicates whether it's a client component.
+ * @param {InquirerDataType} data - Input data.
  */
-function updateIndexFilesIfNeeded(nestedRouteActions, rootSegments, currentDirSegments, isClient) {
+function updateIndexFilesIfNeeded(
+  nestedRouteActions,
+  rootSegments,
+  currentDirSegments,
+  data,
+  pkgJSON,
+) {
   const indexFilePath = path.resolve(__dir, ...rootSegments, ...currentDirSegments, "index.ts");
   const root = rootSegments.join("/");
   if (!fs.existsSync(indexFilePath)) {
     const content =
-      `${isClient ? '"use client";\n' : ""}// ${currentDirSegments.join("/")}` +
+      `${data.isClient ? '"use client";\n' : ""}// ${currentDirSegments.join("/")}` +
       " component exports\n";
+    const dirPath = `${root + currentDirSegments.join("/")}`;
     nestedRouteActions.push({
       type: "add",
-      path: `${root + currentDirSegments.join("/")}/index.ts`,
+      path: `${dirPath}/index.ts`,
       template: content,
     });
     const length = currentDirSegments.length;
@@ -39,6 +61,8 @@ function updateIndexFilesIfNeeded(nestedRouteActions, rootSegments, currentDirSe
       }index.ts`,
       template: `export * from "./${currentDirSegments[length - 1]}"`,
     });
+    // update exports
+    createExportsEntry(pkgJSON, dirPath.split("src/")[1]);
   }
 }
 
@@ -59,7 +83,7 @@ function toKebabCase(str) {
  * createRootIndexAndDeclarations if not present.
  * @param {InquirerDataType} data - Input data.
  */
-function createRootIndexAndDeclarations(data) {
+function createRootIndexAndDeclarations(data, pkgJSON) {
   const nestedRouteActions = [];
   const { isClient } = data;
   const srcDir = path.resolve(__dir, `${data.pkgPath}/src`);
@@ -67,13 +91,25 @@ function createRootIndexAndDeclarations(data) {
   const root = `${data.pkgPath}/src/${target}/`;
 
   /** Create index.ts in src directory if not present.  */
-  if (!fs.existsSync(path.resolve(srcDir, "index.ts")))
+  if (!fs.existsSync(path.resolve(srcDir, "index.ts"))) {
     nestedRouteActions.push({
       type: "add",
       path: `${data.pkgPath}/src/index.ts`,
       template: `${banner}export * from "./${target}";\n`,
     });
-
+    // Create entry in package.json exports field
+    pkgJSON.exports = {
+      ".": {
+        types: "./dist/index.d.ts",
+        import: "./dist/index.mjs",
+        require: "./dist/index.js",
+      },
+      "./index.css": "./dist/index.css",
+      "./dist/index.css": "./dist/index.css",
+      "./styles": "./dist/index.css",
+      "./css": "./dist/index.css",
+    };
+  }
   /** Create declaration if not present.  */
   if (!fs.existsSync(path.resolve(srcDir, "declaration.d.ts")))
     nestedRouteActions.push({
@@ -83,14 +119,16 @@ function createRootIndexAndDeclarations(data) {
     });
 
   /** Create index.ts in src/client or src/server directory if not present.  */
-  if (!fs.existsSync(path.resolve(__dir, root, "index.ts")))
+  if (!fs.existsSync(path.resolve(__dir, root, "index.ts"))) {
     nestedRouteActions.push({
       type: "add",
       path: `${root}index.ts`,
       template: `${banner}/**\n * Server components and client components need to be exported from separate files as\n * directive on top of the file from which component is imported takes effect.\n * i.e., server component re-exported from file with "use client" will behave as client component\n */\n\n// ${target} component exports\n`,
     });
+    createExportsEntry(pkgJSON, target);
+  }
 
-  return { nestedRouteActions, root, isClient };
+  return { nestedRouteActions, root };
 }
 
 /**
@@ -98,9 +136,9 @@ function createRootIndexAndDeclarations(data) {
  * @param {InquirerDataType} data - Input data.
  * @returns {Object} Nested route actions and parent directory.
  */
-function getNestedRouteActions(data) {
+function getNestedRouteActions(data, pkgJSON) {
   const name = data.name.replace(/\/+/g, "/").replace(/\/$/, "").trim();
-  const { nestedRouteActions, root, isClient } = createRootIndexAndDeclarations(data);
+  const { nestedRouteActions, root } = createRootIndexAndDeclarations(data, pkgJSON);
 
   if (!name.includes("/")) return { nestedRouteActions, parentDir: root };
 
@@ -111,7 +149,13 @@ function getNestedRouteActions(data) {
   const rootSegments = [...root.split(/\/|\\/)];
 
   for (let i = 1; i <= directories.length; i++)
-    updateIndexFilesIfNeeded(nestedRouteActions, rootSegments, directories.slice(0, i), isClient);
+    updateIndexFilesIfNeeded(
+      nestedRouteActions,
+      rootSegments,
+      directories.slice(0, i),
+      data,
+      pkgJSON,
+    );
 
   return { nestedRouteActions, parentDir: `${root + directories.join("/")}/` };
 }
@@ -122,8 +166,10 @@ function getNestedRouteActions(data) {
  * @param {string} parentDir - Parent directory.
  * @returns {Object} Index action.
  */
-function getIndexAction(data, parentDir) {
-  const indFilePath = path.resolve(__dir, parentDir, toKebabCase(data.name), "index.ts");
+function getIndexAction(data, parentDir, pkgJSON) {
+  const dirPath = path.resolve(__dir, parentDir, toKebabCase(data.name));
+  console.log("dirpath --- ", dirPath);
+  const indFilePath = path.resolve(dirPath, "index.ts");
   if (fs.existsSync(indFilePath))
     return {
       type: "append",
@@ -131,6 +177,8 @@ function getIndexAction(data, parentDir) {
       path: `${parentDir}{{kebabCase name}}/index.ts`,
       template: 'export * from "./{{kebabCase name}}";',
     };
+  // add exports if file did not exists
+  createExportsEntry(pkgJSON, dirPath.split("src")[1].slice(1).replace(/\\/g, "/"));
   return {
     type: "add",
     path: `${parentDir}{{kebabCase name}}/index.ts`,
@@ -144,10 +192,16 @@ function getIndexAction(data, parentDir) {
  * @returns {Array} Actions.
  */
 function getActions(data) {
-  const { nestedRouteActions, parentDir } = getNestedRouteActions(data);
+  const packageJSONPath = path.resolve(__dirname, "..", data.pkgPath, "package.json");
+  const pkgJSON = JSON.parse(fs.readFileSync(packageJSONPath, "utf8"));
+  const { nestedRouteActions, parentDir } = getNestedRouteActions(data, pkgJSON);
+
+  const indexAction = getIndexAction(data, parentDir, pkgJSON);
+
+  fs.writeFileSync(packageJSONPath, JSON.stringify(pkgJSON, null, 2) + "\n");
 
   return nestedRouteActions.concat([
-    getIndexAction(data, parentDir),
+    indexAction,
     {
       type: "add",
       path: `${parentDir}{{kebabCase name}}/{{kebabCase name}}.tsx`,

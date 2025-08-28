81e725a59c3cfbd869fba126621d6bd15adde814
Mayank <mayank.srmu@gmail.com>
2025-08-28 22:09:27 +0530

    Enhance error handling on canonical publishing

--- scripts/publish-canonical.js
+++ scripts/publish-canonical.js
@@ -2,6 +2,12 @@ const { execSync } = require("child_process");
 
 // Publish canonical packages
 [].forEach(pkg => {
-  execSync(`sed -i -e "s/name.*/name\\": \\"${pkg.replace(/\//g, "\\\\/")}\\",/" lib/package.json`);
-  execSync("cd lib && npm publish --provenance --access public");
+  try {
+    execSync(
+      `sed -i -e "s/name.*/name\\": \\"${pkg.replace(/\//g, "\\\\/")}\\",/" lib/package.json`,
+    );
+    execSync("cd lib && npm publish --provenance --access public");
+  } catch (err) {
+    console.error(`Error publishing ${pkg}: `, err);
+  }
 });
